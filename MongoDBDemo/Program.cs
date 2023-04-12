3f80c632e28d61b8b331b4ef056e726ad008753a
Alif Muqri Hazmi <alifmuqri.hazmi@petronas.com>
2023-04-12 12:45:18 +0800

    connect to db and find the enterprise_id in the collection

--- MongoDBDemo/Program.cs
+++ MongoDBDemo/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDBDemo;
+
+string connectionString = "MONGODB CONNECTION HERE";
+
+if(connectionString == null)
+{
+    Console.WriteLine("Connection Failed");
+    Environment.Exit(0);
+}
+
+string databaseName = "upmobilecosmos";
+string collectionName = "users";
+
+var client = new MongoClient(connectionString);
+var db = client.GetDatabase(databaseName);
+var collection = db.GetCollection<BsonDocument>("users");
+
+var filter = Builders<BsonDocument>.Filter.Eq("enterprise_id", "mnazrulhisham.mohama");
+
+var document = collection.Find(filter).First();
+
+if(document == null)
+{
+    Console.WriteLine("No records found.");
+}
+else
+{
+    Console.WriteLine(document.ToJson());
+}
